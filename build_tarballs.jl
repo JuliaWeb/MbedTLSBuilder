051a1df473cb50924214a869b8339d85d8b8e7ed
quinnj <quinn.jacobd@gmail.com>
2018-09-19 16:50:08 -0600

    Avoid building musl for now

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -24,7 +24,7 @@ fi
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = Platform[Linux(:i686, :glibc), Linux(:x86_64, :glibc), Linux(:aarch64, :glibc), Linux(:armv7l, :glibc, :eabihf), Linux(:powerpc64le, :glibc), MacOS(:x86_64), FreeBSD(:x86_64), Windows(:i686), Windows(:x86_64)]
 
 # The products that we will ensure are always built
 products(prefix) = [
